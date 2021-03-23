55a79008355859a42a54b69495083aea4f363f07
sabithamallela <sabitha.mallela@gmail.com>
2021-03-23 22:31:46 +0000

    initial commit

---
+++ src/test/java/org/gitpractise/first/HelloWorld.java
@@ -0,0 +1,20 @@
+package org.gitpractise.first;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class HelloWorld
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
